e4d63a109d7f8d7decbdc73d0e67993146a67eb8
Mokus_Proffeszor <exodia1114@gmail.com>
2025-10-21 21:05:20 +0200

    Initial commit

---
+++ src/main/java/net/mokus/halloween/MokusSpookySeason.java
@@ -0,0 +1,15 @@
+package net.mokus.halloween;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MokusSpookySeason implements ModInitializer {
+	public static final String MOD_ID = "mokus_halloween";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/mokus/halloween/MokusSpookySeasonClient.java
@@ -0,0 +1,14 @@
+package net.mokus.halloween;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MokusSpookySeasonClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
